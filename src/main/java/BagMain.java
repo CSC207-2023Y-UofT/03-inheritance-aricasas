fdf4953c10f5aa405f4261b84d100e2a13df5afc
Ari <50893284+aricasas@users.noreply.github.com>
2023-06-11 19:49:38 -0400

    Update BagMain.java

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -19,7 +19,7 @@ class BagMain {
         for (Bag bag : bags) {
             bag.enhance();
 
-            if (double_enhance_handbags) {
+            if (double_enhance_handbags  && bag instanceof HandBag) {
                 bag.enhance();
             }
         }
